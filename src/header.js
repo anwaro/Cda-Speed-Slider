c2f1b4c94b704c17cf38811403752c76d6187732
Łukasz Micał <lukasz.mical@sarigato.com>
2017-10-24 19:07:42 +0200

    Initial commit - old version

---
+++ src/header.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Cda Speed Slider
+// @version      0.1.2
+// @namespace    http://lukaszmical.pl/
+// @description  Cda.pl Speed Slider
+// @author       Łukasz
+// @include      https://www.cda.pl/video/*
+// @grant        none
+// ==/UserScript==
